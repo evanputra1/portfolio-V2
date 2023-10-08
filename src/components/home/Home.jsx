6dac44f07db9f91c347e52f04d162c25192a5dce
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-10-08 23:11:05 +0700

    add file data.jsx and add some style home.css

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,16 @@
+import "./home.css";
+import Social from "./Social";
+import Data from "./Data";
+
 const Home = () => {
   return (
     <section className="home section" id="home">
       <div className="home__container container grid">
-        <div className="home__content grid"></div>
+        <div className="home__content grid">
+          <Social />
+          <div className="home__img"></div>
+          <Data />
+        </div>
       </div>
     </section>
   )
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,7 +1,15 @@
 const Social = () => {
   return (
     <div className="home__social">
-      <a href="" className="home__social-icon"></a>
+      <a href="https://www.instagram.com/evan._putra" className="home__social-icon" target="_blank" rel="noreferrer">
+        <i className='uil uil-instagram'></i>
+      </a>
+      <a href="https://www.linkedin.com/in/evan-ananda-putra-056176222/" className="home__social-icon" target="_blank" rel="noreferrer">
+        <i className='uil uil-linkedin-alt'></i>
+      </a>
+      <a href="https://github.com/evanputra1" className="home__social-icon" target="_blank" rel="noreferrer">
+        <i className='uil uil-github-alt'></i>
+      </a>
     </div>
   )
 }
