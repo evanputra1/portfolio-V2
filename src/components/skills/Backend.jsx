cb878a6a6cca178bf759e948f8d20083e7c2e660
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-10-10 01:00:14 +0700

    add skills components

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,53 @@
+const Backend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Backend Developer</h3>
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">PHP</h3>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Node Js</h3>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">MySQL</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Firebase</h3>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">MongoDB</h3>
+            </div>
+          </div>
+          <div className="skills__data">
+            <i className="bx bx-badge-check"></i>
+            <div>
+              <h3 className="skills__name">Express JS</h3>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Backend
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import "./skills.css";
+import Frontend from "./Frontend";
+import Backend from "./Backend";
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <section className="skills section" id="skills">
+      <h2 className="section__title">Skills</h2>
+      <span className="section__subtitle">My Technical Level</span>
+
+      <div className="skills__container container grid">
+        <Frontend />
+        <Backend />
+      </div>
+    </section>
   )
 }
 
