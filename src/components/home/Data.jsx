f107178af9f5a18b707040e79041e5b010f30e44
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-10-09 21:17:08 +0700

    add home components

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -1,7 +1,7 @@
 const Data = () => {
   return (
     <div className="home__data">
-      <h1 className="home__title">Evan Ananda Putra
+      <h1 className="home__title">Hi, I&apos;am Evan
         <svg width="36" height="36" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" className="home__hand">
           <path d="M25.4995 32.0305L31.3495 33.1555L36.1495 8.48051C36.4495 6.83051 35.3995 5.18051 33.8245 4.88051C32.1745 4.58051 30.5995 5.70551 30.2995 7.35551L25.4995 32.0305Z" fill="#FFDD67" ></path>
           <path d="M33.8996 4.88018C33.6746 4.80518 33.5246 4.80518 33.2996 4.80518C34.6496 5.33018 35.3996 6.75518 35.0996 8.25518L30.2996 32.9302L31.3496 33.1552L36.1496 8.48018C36.5246 6.75518 35.4746 5.18018 33.8996 4.88018Z" fill="#EBA352" ></path>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,7 @@
 import "./home.css";
 import Social from "./Social";
 import Data from "./Data";
+import ScrollDown from "./ScrollDown";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
           <div className="home__img"></div>
           <Data />
         </div>
+        <ScrollDown />
       </div>
     </section>
   )
