ec1898145b436045dd6748aa8b2b6a11c3497242
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-10-11 18:50:53 +0700

    membuat footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer">
         <div className="footer__container container">
-            <h1 className="footer__title">Evan</h1>
+            <h1 className="footer__title">Evan Ananda Putra</h1>
             <ul className="footer__list">
                 <li>
                     <a href="#about" className="footer__link">About</a>
@@ -13,9 +13,23 @@ const Footer = () => {
                     <a href="#portfolio" className="footer__link">Projects</a>
                 </li>
                 <li>
-                    <a href="" className="footer__link">Testimonials</a>
+                    <a href="#testimonials" className="footer__link">Testimonials</a>
                 </li>
             </ul>
+
+            <div className="footer__social">
+                <a href="https://www.instagram.com/evan._putra" className="footer__social-link" target="_blank" rel="noreferrer">
+                    <i className='bx bxl-facebook'></i>
+                </a>
+                <a href="https://www.linkedin.com/in/evan-ananda-putra-056176222/" className="footer__social-link" target="_blank" rel="noreferrer">
+                    <i className='bx bxl-instagram'></i>
+                </a>
+                <a href="https://github.com/evanputra1" className="footer__social-link" target="_blank" rel="noreferrer">
+                    <i className='bx bxl-twitter'></i>
+                </a>
+            </div>
+
+            <span className='footer__copy'>&#169; Evan Ananda Putra. All Rights Reserved</span>
         </div>
     </footer>
   )
---
+++ src/components/scrollup/ScrollUp.jsx
@@ -0,0 +1,11 @@
+import './scrollup.css';
+
+const ScrollUp = () => {
+  return (
+    <a href="#" className="scrollup">
+        <i className="uil uil-arrow-up scrollup__icon"></i>
+    </a>
+  )
+}
+
+export default ScrollUp
